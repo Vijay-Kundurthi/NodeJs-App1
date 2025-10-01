b3028c8cc8e01c769aa68e20e34f27fa943b4d2c
Vijay-Kundurthi <vijaykundurthi02@gmail.com>
2025-10-01 17:38:51 +0530

    Remove node_modules from git tracking

--- express-js/swagger-report.js
+++ express-js/swagger-report.js
@@ -1,50 +1,7 @@
-// const express = require('express');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerJSDoc = require('swagger-jsdoc');
-// const userRouter = require('./route/user-router');
-
-// const app = express();
-// app.use(express.json());
-
-// // Swagger configuration
-// const swaggerDefinition = {
-//   openapi: '3.0.0',
-//   info: {
-//     title: 'My Express API',
-//     version: '1.0.0',
-//     description: 'A simple Express API with Swagger documentation',
-//   },
-//   servers: [
-//     {
-//       url: 'http://localhost:3000',
-//     },
-//   ],
-// };
-
-// const options = {
-//   swaggerDefinition,
-//   apis: ['./route/user-router.js'], // 👈 Path to your route files with Swagger comments
-// };
-
-// const swaggerSpec = swaggerJSDoc(options);
-
-// // Serve Swagger docs at /api-docs
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// //
-// app.use('/user/v1', userRouter);
-
-// // Start server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-//   console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
-// });
-
-// app.js
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
+const userRouter = require('./routes/user-router');
 
 const app = express();
 app.use(express.json());
@@ -66,7 +23,7 @@ const swaggerDefinition = {
 
 const options = {
   swaggerDefinition,
-  apis: ['./route/*.js'], // 👈 Path to your route files with Swagger comments
+  apis: ['./routes/*.js'], // 👈 Path to your route files with Swagger comments
 };
 
 const swaggerSpec = swaggerJSDoc(options);
@@ -74,10 +31,8 @@ const swaggerSpec = swaggerJSDoc(options);
 // Serve Swagger docs at /api-docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Sample route
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
-});
+//
+app.use('/user/v1', userRouter);
 
 // Start server
 const PORT = process.env.PORT || 3000;
